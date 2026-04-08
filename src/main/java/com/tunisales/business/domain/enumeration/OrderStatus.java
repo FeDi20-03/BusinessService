7523d0f29399f1cc56aae147f583d6e75fd29915
FeDi20-03 <fedi.chiha2003@gmail.com>
2026-04-08 15:38:25 +0200

    init

---
+++ src/main/java/com/tunisales/business/domain/enumeration/OrderStatus.java
@@ -0,0 +1,18 @@
+package com.tunisales.business.domain.enumeration;
+
+/**
+ * The OrderStatus enumeration.
+ */
+public enum OrderStatus {
+    DRAFT,
+    SUBMITTED,
+    UNDER_REVIEW,
+    APPROVED,
+    IN_PREPARATION,
+    SHIPPED,
+    DELIVERED,
+    INVOICED,
+    PAID,
+    REJECTED,
+    CANCELLED,
+}
